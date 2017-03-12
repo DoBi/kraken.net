c38e4800e97edd0e0afb17cc908fc9b27231af57
Dominik Bittner <DoBi-tyndur@gmx.net>
2017-03-12 16:05:32 +0100

    Add new tests for GetServerTime and calling an unknown method.
    Signed-off-by: Dominik Bittner <DoBi-tyndur@gmx.net>

--- test/Kraken.Tests/ApiTest.cs
+++ test/Kraken.Tests/ApiTest.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 
@@ -25,7 +27,26 @@ namespace Kraken.Tests
                 new HttpResponseMessage(HttpStatusCode.OK)
                 {
                     Content = new StringContent(File.ReadAllText("Responses/Assets.json"))
-                });
+                }
+            );
+
+            handler.AddResponse(
+                new Uri(String.Format("{0}/{1}/public/{2}", Api.Url, Api.Version, "Time")), 
+                String.Empty, 
+                new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(File.ReadAllText("Responses/ServerTime.json"))
+                }
+            );
+            
+            handler.AddResponse(
+                new Uri(String.Format("{0}/{1}/public/{2}", Api.Url, Api.Version, "Tim")),
+                String.Empty,
+                new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(File.ReadAllText("Responses/ErrorGeneral.json"))
+                }
+            );
         }
 
         [Fact]
@@ -36,6 +57,45 @@ namespace Kraken.Tests
             Assert.True(assets.Count > 0);
 
             Assert.Contains(assets, a => a.Name.Equals("EUR"));
+        }
+
+        [Fact]
+        public void TestGetServerTime()
+        {
+            var time = _api.GetServerTime();
+            var utc = time.ToUniversalTime();
+
+            Assert.Equal(12, utc.Day);
+            Assert.Equal(3, utc.Month);
+            Assert.Equal(2017, utc.Year);
+            Assert.Equal(14, utc.Hour);
+            Assert.Equal(48, utc.Minute);
+            Assert.Equal(43, utc.Second);
+        }
+
+        [Fact]
+        public void TestGeneralError()
+        {
+            IList<Error> errors = new List<Error>();
+
+            try 
+            {
+                var assets = _api.QueryPublicAsync("Tim", null).Result;
+            } 
+            catch (AggregateException ex)
+            {
+                Assert.IsType(typeof(KrakenException), ex.InnerException);
+
+                errors = ((KrakenException) ex.InnerException).Errors;
+            }
+
+            Assert.Equal(1, errors.Count());
+            Error error = errors.FirstOrDefault();
+            Assert.NotNull(error);
+            Assert.Equal("General", error.Category);
+            Assert.Equal(Error.Severity.Error, error.SeverityCode);
+            Assert.Equal("Unknown method", error.ErrorType);
+            Assert.Equal(null, error.ExtraInfo);
         }
     }
 }
